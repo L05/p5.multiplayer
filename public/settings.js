ce7468f0e9c49957c3c49590e177c6b8e0789e51
L05 <carlosga@gmail.com>
2018-02-21 23:46:42 -0800

    Added Heroku support.

--- public/settings.js
+++ public/settings.js
@@ -4,7 +4,7 @@
 const serverIp			= 'http://l05-play.herokuapp.com';	  // Server IP address
 // const serverIp			= 'http://L05.play.ai';	  // Server IP address
 // const serverIp      = 'http://10.0.1.16';  // Server IP address
-const serverPort		= '3000';                 // Server port
+const serverPort		= '80';                 // Server port
 const rotationMode  = false;
 const touchMode			= 2;					            // 0 = touchPad, 1 = octoPad, 2 = segTrackPad
 const segResolution = 3;                      // segTrackPad resolution
