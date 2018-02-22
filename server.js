c1c3dd8c477c4fd9fde788d486202f23bfc9f2f7
L05 <carlosga@gmail.com>
2018-02-21 22:54:00 -0800

    Tweaked server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ let screens	= [];
 let express = require('express');
 let app = express();
 let server = app.listen(3000);
+let port=Number(process.env.PORT || 3000);
 
 app.use(express.static('public'));
 
