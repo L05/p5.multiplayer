140f059764dbca4d098d3479edb4b6bfcc76cc49
L05 <carlosga@gmail.com>
2019-09-01 15:43:24 -0700

    Externalized core functionality.

--- template/index.js
+++ template/index.js
@@ -1,6 +1,5 @@
 /*
-Author:	L05
-Date:	2019.08.31
+p5.multiplayer - CLIENT
 
 This 'client' sketch is intended to be run in either mobile or 
 desktop browsers. It sends a basic joystick and button input data 
@@ -14,145 +13,53 @@ Run http-server -c-1 -p80 to start server on open port 80.
 */
 
 ////////////
-// Socket Network Settings
+// Network Settings
 // const serverIp      = 'https://yourservername.herokuapp.com';
 const serverIp      = '127.0.0.1';
 const serverPort    = '3000';
 const local         = true;   // true if running locally, false
                               // if running on remote server
-let socket;
 
-// Initialize Network related variables
-let roomId = null;
-let waiting = true;
-let connected = false;
-let id = null;
+// Global variables here. ---->
 
-////////////////////////////////////////
-////////////////////////////////////////
+// <----
 
-
-////////////
-// Setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  processUrl();
-  
-  // Put your own client setup logic here. ---->
 
+  setupClient();
+
+  // Client setup here. ---->
 
   // <----
 
-  // Socket.io - open a connection to the web server on specified port
-  let addr = serverIp;
-  if (local) { addr = serverIp + ':' + serverPort; }
-  socket = io.connect(addr);
+  // Send any initial setup data to your host here.
+  /* Example: 
+     sendData({
+       type: 'playerColor', 
+       r: red(playerColor)/255,
+       g: green(playerColor)/255,
+       b: blue(playerColor)/255
+     });
 
-  socket.emit('join', {name: 'client', roomId: roomId});
-
-  socket.on('id', function(data) {
-    id = data;
-    console.log("id: " + id);
-  });
-
-  socket.on('found', function(data) {
-    connected = data.status;
-    waiting = false;
-    console.log("connected: " + connected);
-  })
-  
-  socket.emit('clientConnect', {
-    roomId: roomId
-  });
-
-  // You can send any initial setup data to your host here.
-  // Example: 
-  // sendData({
-  //   type: 'playerColor', 
-  //   r: red(playerColor)/255,
-  //   g: green(playerColor)/255,
-  //   b: blue(playerColor)/255
-  // });
-} 
-
-////////////
-// Process URL
-// Used to process the room ID. In order to specify a room ID,
-// include ?=uniqueName, where uniqueName is replaced with the 
-// desired unique room ID.
-function processUrl() {
-  const parameters = location.search.substring(1).split("&");
-
-  const temp = parameters[0].split("=");
-  roomId = unescape(temp[1]);
-
-  console.log("id: " + roomId);
+     Use `type` to classify message types for host.
+  */
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-////////////
-// Draw loop
 function draw() {
   background(0);
 
-  if (waiting) {
-    displayWaiting();
-    return;
-  } 
-  else if (!connected) {
-    displayInstructions();
-    return;
-  }
-  else {
-    // Put your client code here. ---->
+  if(isConnected(display=true)) {
+    // Client draw here. ---->
 
 
     // <----
   }
 }
-
-////////////
-// Send data to server
-function sendData(data) {
-  // print data to console
-  console.log('Sending: ' + data);
-
-  data.roomId = roomId;
-  
-  // Send rotation data to server
-  socket.emit('sendData', data);
-}
-
-////////////
-// HUD Displays
-
-// Displays a message while attempting connection
-function displayWaiting() {
-  push();
-    fill(200);
-    textAlign(CENTER, CENTER);
-    textSize(20);
-    text("Attempting connection...", width/2, height/2-10);
-  pop();
-}
-
-// Displays a message instructing player to look at host screen 
-// for correct link.
-function displayInstructions() {
-  push();
-    fill(200);
-    textAlign(CENTER, CENTER);
-    textSize(20);
-    text("Please enter the link at the", width/2, height/2-10);
-    text("bottom of the host screen.", width/2, height/2+10);
-  pop();
-}
-
-////////////////////////////////////////
-////////////////////////////////////////
 
 /// Add these lines below sketch to prevent scrolling on mobile
 function touchMoved() {
---
+++ template/lib/p5.multiplayer.js
@@ -0,0 +1,149 @@
+// Initialize Network related variables
+let socket;
+let roomId          = null;
+let waiting         = true;
+let connected       = false;
+let id              = null;
+let hostConnected   = false;
+
+
+////////////
+// SETUP
+
+function setupClient() {
+  _processUrl();
+
+  // Socket.io - open a connection to the web server on specified port
+  let addr = serverIp;
+  if (local) { addr = serverIp + ':' + serverPort; }
+  socket = io.connect(addr);
+
+  socket.emit('join', {name: 'client', roomId: roomId});
+
+  socket.on('id', function(data) {
+    id = data;
+    console.log("id: " + id);
+  });
+
+  socket.on('found', function(data) {
+    connected = data.status;
+    waiting = false;
+    console.log("connected: " + connected);
+  })
+  
+  socket.emit('clientConnect', {
+    roomId: roomId
+  });
+}
+
+function setupHost() {
+  _processUrl();
+
+  let addr = serverIp;
+  if (local) { addr = serverIp + ':' + serverPort; }
+  socket = io.connect(addr);
+
+  socket.emit('join', {name: 'host', roomId: roomId});
+
+  socket.on('hostConnect', onHostConnect);
+  socket.on('clientConnect', onClientConnect);
+  socket.on('clientDisconnect', onClientDisconnect);
+  socket.on('receiveData', onReceiveData);
+}
+
+function onHostConnect (data) {
+  console.log("Host connected to server.");
+  hostConnected = true;
+  
+  if (roomId === null || roomId === 'undefined') {
+    roomId = data.roomId;
+  }
+}
+
+// Process URL
+// Used to process the room ID. In order to specify a room ID,
+// include ?=uniqueName, where uniqueName is replaced with the 
+// desired unique room ID.
+function _processUrl() {
+  const parameters = location.search.substring(1).split("&");
+
+  const temp = parameters[0].split("=");
+  roomId = unescape(temp[1]);
+
+  console.log("id: " + roomId);
+}
+
+
+////////////
+// MESSAGING
+
+// Send data from client to host via server
+function sendData(data) {
+  // print data to console
+  console.log('Sending: ' + data);
+
+  data.roomId = roomId;
+  
+  // Send rotation data to server
+  socket.emit('sendData', data);
+}
+
+
+////////////
+// FLOW CONTROL
+
+function isConnected(display=false) {
+  if (waiting) {
+    if (display) { _displayWaiting(); }
+    return false;
+  } 
+  else if (!connected) {
+    if (display) { _displayInstructions(); }
+    return false;
+  }
+
+  return true;
+}
+
+function isHostConnected(display=false) {
+  if (!hostConnected) {
+    if (display) { _displayWaiting(); }
+    return false;
+  }
+  return true;
+}
+
+
+////////////
+// ONSCREEN DISPLAY
+
+// Displays a message while attempting connection
+function _displayWaiting() {
+  push();
+    fill(200);
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    text("Attempting connection...", width/2, height/2-10);
+  pop();
+}
+  
+// Displays a message instructing player to look at host screen 
+// for correct link.
+function _displayInstructions() {
+  push();
+    fill(200);
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    text("Please enter the link at the", width/2, height/2-10);
+    text("bottom of the host screen.", width/2, height/2+10);
+  pop();
+}
+
+// Displays server address in lower left of screen
+function displayAddress() {
+  push();
+    fill(255);
+    textSize(50);
+    text(serverIp+"/?="+roomId, 10, height-50);
+  pop();
+}
